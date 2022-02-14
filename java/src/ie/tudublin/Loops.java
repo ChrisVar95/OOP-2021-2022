1eb1ca29b9fc1b72a6dac8e9f6bf3a4fc949149f
ChrisVar95 <christina.vargka@gmail.com>
2022-02-14 11:44:15 +0000

    further changes made

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -15,21 +15,33 @@ public class Loops extends PApplet
 		background(0);
 		top = 50;
 		center = 250;
-		width = 200;
+		wid = 200;
 	}
-	float top, center, width;
+	float top, center, wid;
 	int mode = 0;
 	
-	public void keyPressed(){
-		if (mousePressed == true) {
-			if (mode < 9) {
-				mode++;
-			}else {
-				mode = 0;
-			}
+	public void mousePressed(){
+		mode = (mode +1) % 9;
+	}
+
+	float MagicMap(float i,float a,float b,float c,float d){
+		float output;
+		i -= a;
+		b -= a;
+		d -= c;
+
+		output = ((i/b)*d) +c;
+		return output;
+		/* 
+		map(a, b, c, d, e){
+			r1 = c - b
+			r2 = e - d
+			return = (((a - b) / r1) * r2) + d
 		}
+		*/
 	}
 	
+
 	public void draw()
 	{	
 		background(255, 0, 0);
@@ -38,17 +50,27 @@ public class Loops extends PApplet
 		
 		switch (mode) {
 			case 0: if (mouseX <= center){
-						rect(top, top, width, width*2);
+						rect(top, top, wid, wid*2);
 					}else{
-						rect(center, top, width, width*2);
+						rect(center, top, wid, wid*2);
 					}fill(130,255,255);
 					break;
 			
-			case 1: if (mouseX <= center){
-						rect(top, top, width, width*2);
-					}else{
-						rect(center, top, width, width*2);
-					}fill(100,255,255);
+			case 1: //
+					int bars = 10;
+					float w = width / (float) bars;
+					for (int i = 0; i < bars; i++){
+						noStroke();
+						fill(map(i, 0, 10, 0, 255), 255, 255);
+						rect(map(i, 0, 10, 0, 500), 0, w, height);
+					}
+					break;
+			case 2:
+					//map(a,b,c,d,e);
+					//a=inputvalue
+					//b-c - start and end of the first range
+					//d, e 0 - start and and of the end range
+					map(-2, 10, 90, 200, 233);
 					break;
 		}
 		
