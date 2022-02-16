c3314cd43eb99d00135d9d763c4283ba92751575
ChrisVar95 <christina.vargka@gmail.com>
2022-02-16 22:37:49 +0000

    boxes finished

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -13,23 +13,20 @@ public class Loops extends PApplet
 	public void setup() {
 		colorMode(HSB);
 		background(0);
-		top = 50;
-		center = 250;
-		wid = 200;
 	}
 	float top, center, wid;
-	int mode = 0;
+	int mode = 7;
 	
 	public void mousePressed(){
 		mode = (mode +1) % 9;
 	}
-
+	
 	float MagicMap(float i,float a,float b,float c,float d){
 		float output;
 		i -= a;
 		b -= a;
 		d -= c;
-
+		
 		output = ((i/b)*d) +c;
 		return output;
 		/* 
@@ -41,39 +38,58 @@ public class Loops extends PApplet
 		*/
 	}
 	
-
+	
 	public void draw()
 	{	
 		background(255, 0, 0);
 		//rectMode(CENTER);
 		
 		
-		switch (mode) {
-			case 0: if (mouseX <= center){
-						rect(top, top, wid, wid*2);
-					}else{
-						rect(center, top, wid, wid*2);
-					}fill(130,255,255);
-					break;
+		switch (mode) { 
+			case 5: {// 2 reactive boxes
+				top = 50;
+				center = 250;
+				wid = 200;
+				if (mouseX <= center){
+					rect(top, top, wid, wid*2); //x, y, w, h
+				}else{
+					rect(center, top, wid, wid*2);
+				}fill(130,255,255);
+				break;
+			}
+					
 			
-			case 1: //
-					int bars = 10;
-					float w = width / (float) bars;
-					for (int i = 0; i < bars; i++){
-						noStroke();
-						fill(map(i, 0, 10, 0, 255), 255, 255);
-						rect(map(i, 0, 10, 0, 500), 0, w, height);
-					}
-					break;
-			case 2:
-					//map(a,b,c,d,e);
-					//a=inputvalue
-					//b-c - start and end of the first range
-					//d, e 0 - start and and of the end range
-					map(-2, 10, 90, 200, 233);
-					break;
+			case 6: {//4 reactive boxes
+				top = 50;
+				center = 250;
+				wid = 200;
+				if (mouseX <= center && mouseY <= center){
+					rect(top, top, wid, wid);
+				}else if (mouseX <= center && mouseY >= center){
+					rect(top, center, wid, wid);
+				}else if (mouseX >= center && mouseY <= center){
+					rect(center, top, wid, wid);
+				}else{
+					rect(center, center, wid, wid);
+				}fill(130,255,255);
+				break;
+			}
+
+			case 7: {// 1 reactive box change color
+				top = 50;
+				center = height / 2.0f;
+				wid = 200;
+				float boxX = center - (wid/2.0f);
+				float boxY = center - top;
+				rect(boxX, boxY, wid, top); //x, y, w, h
+				if (mouseX >= boxX && mouseX <= boxX + wid && mouseY >= boxY && mouseY <= boxY + top){
+					fill(0,255,255);
+				}else{
+					fill(130,255,255);
+				}
+				break;
+			}
 		}
-		
 
 	}
 }
